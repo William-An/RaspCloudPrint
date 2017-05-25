1e688c13b352fccfe07747026e8f77332b2467f2
Weili An <China_Aisa@live.com>
2017-05-25 09:33:25 +0800

    Finish project

---
+++ server.py
@@ -0,0 +1,69 @@
+import web
+import os
+import sys
+import subprocess
+import datetime
+# Log page! 
+urls = ('/upload', 'Upload',
+		'/error','Err',
+		'/log','Log')
+ 
+class Upload:
+    def GET(self):
+        web.header("Content-Type","text/html; charset=utf-8")
+        render = web.template.render('static/')
+        return render.index()
+
+    def POST(self):
+        x = web.input(printjob={})
+        filedir = 'file'                                 	# change this to the directory you want to store the file in.
+        if 'printjob' in x:                                   # to check if the file-object is created
+            filepath=x.printjob.filename.replace('\\','/')    # replaces the windows-style slashes with linux ones.
+            filename=filepath.split('\\')[-1]               # splits the and chooses the last part (the filename with extension)
+            fout = open(filedir +'\\'+ filename,'wb')       # creates the file where the uploaded file should be stored
+            fout.write(x.printjob.file.read())                # writes the uploaded file to the newly created file.
+            fout.close()                                    # closes the file, upload complete.
+        filename = filedir+"\\"+filename
+        if 'doc' or 'pdf' in filename[-4:]:                
+            printer(filename)
+        else:
+        	raise web.seeother('/error')
+        # else if 'pdf'in filename[-4:]: TODO finish pdf printing
+        #     doc_print(filename,printer='arco')
+class Err:
+	def GET(self):
+		web.header("Content-Type","text/html; charset=utf-8")
+		render = web.template.render('static/')
+		return render.error()
+
+class Log:
+	def GET(self):
+		with open("static/log") as logfile:
+			return "<a href=\"static/log\">Download Log</a><br>"+"<br>".join(logfile.readlines());
+def notfound():
+	raise web.seeother('/error')
+
+def printer(filename):
+	with open('static/log','a+') as logfile:
+		logfile.write("[*] Start printjob "+filename+" "+str(datetime.datetime.now())+"\n")
+		try:
+			subprocess.run(['lp',filename],timeout=30)
+			os.system("rm "+filename)
+			logfile.write("[+] Finish printjob "+filename+" "+str(datetime.datetime.now())+"\n")
+			raise web.seeother('/upload')
+		except Exception as err:
+			os.system("rm "+filename)
+			logfile.write("[-] Error in printing "+filename+" "+str(datetime.datetime.now())+" | Error:"+str(err)+"\n")   
+			# print(str(err))
+			raise web.seeother('/error')
+
+if __name__ == "__main__":
+    app = web.application(urls, globals()) 
+    app.notfound = notfound
+    app.internalerror = notfound
+    app.run()
+
+
+
+                        
+
