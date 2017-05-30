aa7eb06e87dea5f56b282819075661ece0014a09
Weili An <China_Aisa@live.com>
2017-05-30 13:07:21 +0000

    Finish basic function

--- server.py
+++ server.py
@@ -18,18 +18,16 @@ class Upload:
         x = web.input(printjob={})
         filedir = 'file'                                 	# change this to the directory you want to store the file in.
         if 'printjob' in x:                                   # to check if the file-object is created
-            filepath=x.printjob.filename.replace('\\','/')    # replaces the windows-style slashes with linux ones.
-            filename=filepath.split('\\')[-1]               # splits the and chooses the last part (the filename with extension)
-            fout = open(filedir +'\\'+ filename,'wb')       # creates the file where the uploaded file should be stored
+            filepath=x.printjob.filename    # replaces the windows-style slashes with linux ones.
+            filename=filepath.split('/')[-1]               # splits the and chooses the last part (the filename with extension)
+            fout = open(filedir+'/'+filename,'wb')       # creates the file where the uploaded file should be stored
             fout.write(x.printjob.file.read())                # writes the uploaded file to the newly created file.
             fout.close()                                    # closes the file, upload complete.
-        filename = filedir+"\\"+filename
-        if 'doc' or 'pdf' in filename[-4:]:                
+        filename = filedir+"/"+filename
+        if 'pdf' in filename[-4:]:                
             printer(filename)
         else:
-        	raise web.seeother('/error')
-        # else if 'pdf'in filename[-4:]: TODO finish pdf printing
-        #     doc_print(filename,printer='arco')
+            raise web.seeother('/error')
 class Err:
 	def GET(self):
 		web.header("Content-Type","text/html; charset=utf-8")
@@ -38,8 +36,8 @@ class Err:
 
 class Log:
 	def GET(self):
-		with open("static/log") as logfile:
-			return "<a href=\"static/log\">Download Log</a><br>"+"<br>".join(logfile.readlines());
+		with open("static/log","rb") as logfile:
+			return "<a href=\"static/log\">Download Log</a><br>"+"<br>".join([i.decode() for i in logfile.readlines()]);
 def notfound():
 	raise web.seeother('/error')
 
@@ -47,12 +45,14 @@ def printer(filename):
 	with open('static/log','a+') as logfile:
 		logfile.write("[*] Start printjob "+filename+" "+str(datetime.datetime.now())+"\n")
 		try:
-			subprocess.run(['lp',filename],timeout=30)
-			os.system("rm "+filename)
+			subprocess.call(['lp',filename],timeout=10)
+			subprocess.call(["rm",filename],timeout=10)
 			logfile.write("[+] Finish printjob "+filename+" "+str(datetime.datetime.now())+"\n")
 			raise web.seeother('/upload')
 		except Exception as err:
-			os.system("rm "+filename)
+			if str(err) == "303 See Other":
+                            raise web.seeother('/upload')
+			subprocess.call(["rm",filename],timeout=10)
 			logfile.write("[-] Error in printing "+filename+" "+str(datetime.datetime.now())+" | Error:"+str(err)+"\n")   
 			# print(str(err))
 			raise web.seeother('/error')
